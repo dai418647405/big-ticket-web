1319b9cc7de1c9319eb44bdcd6be83955fed0b5d
daijing03 <daijing03@meituan.com>
2017-06-07 19:37:33 +0800

    1.升级webmagic core版本 2.增加代理ip爬取选项 3.支持动态开启/关闭爬虫

--- big-ticket-web/src/main/java/org/daijing/big/ticket/crawler/hupu/spider/HupuSpider.java
+++ big-ticket-web/src/main/java/org/daijing/big/ticket/crawler/hupu/spider/HupuSpider.java
@@ -11,11 +11,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Async;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.downloader.HttpClientDownloader;
 import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.proxy.Proxy;
+import us.codecraft.webmagic.proxy.SimpleProxyProvider;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by daijing03 on 17/5/26.
@@ -48,9 +56,73 @@ public class HupuSpider implements SpiderScheduler {
     @Setter
     private Integer limitPageNumber;
 
+    private HttpClientDownloader httpClientDownloader = new HttpClientDownloader();
+
+    private static final int maxProxyNum = 5;
+
+    public HupuSpider() {}
+
+    private void setProxyProvider() {
+        BufferedReader proxyIpBr = null;
+        try {
+            proxyIpBr = new BufferedReader(new FileReader("/data/appdatas/big-ticket-web/proxy_ip.csv"));
+            String line;
+            String[] ipAndPort;
+            List<Proxy> proxyList = new ArrayList<Proxy>(maxProxyNum);
+            while ((line = proxyIpBr.readLine()) != null) {
+                ipAndPort = line.split(":");
+                proxyList.add(new Proxy(ipAndPort[0], Integer.valueOf(ipAndPort[1])));
+            }
+            if (!proxyList.isEmpty()) {
+                httpClientDownloader.setProxyProvider(SimpleProxyProvider.from(proxyList.toArray(new Proxy[proxyList.size()])));
+            } else {
+                logger.error("代理ip为空!将使用本地ip爬取!");
+                httpClientDownloader.setProxyProvider(null);
+            }
+        } catch (Exception e) {
+            logger.error("设置代理ip失败", e);
+            httpClientDownloader.setProxyProvider(null);
+        } finally {
+            if (proxyIpBr != null) {
+                try {
+                    proxyIpBr.close();
+                } catch (IOException e) {
+                    logger.error("关闭代理文件reader异常", e);
+                }
+            }
+        }
+    }
+
+    private boolean permitCrawl() {
+        BufferedReader permitCrawlBr = null;
+        try {
+            permitCrawlBr = new BufferedReader(new FileReader("/data/appdatas/big-ticket-web/permit_crawl.csv"));
+            String line = permitCrawlBr.readLine();
+            if (line != null && !line.trim().equals("")) {
+                return Boolean.parseBoolean(line);
+            }
+        } catch (Exception e) {
+            logger.error("查询是否允许爬取失败,默认允许", e);
+        } finally {
+            if (permitCrawlBr != null) {
+                try {
+                    permitCrawlBr.close();
+                } catch (IOException e) {
+                    logger.error("关闭爬取标志文件reader异常", e);
+                }
+            }
+        }
+        return true;
+    }
+
     @Async
     @Override
     public void schedule() {
+        if (!permitCrawl()) {
+            logger.error("禁止爬取!");
+            return;
+        }
+        this.setProxyProvider();
         DateFormat df = new SimpleDateFormat("mm");
         int minutes = Integer.parseInt(df.format(new Date()));
         ArticleListPageProcessor listPageProcessor = (ArticleListPageProcessor)processor;
@@ -64,7 +136,9 @@ public class HupuSpider implements SpiderScheduler {
         Spider voteSpider = Spider.create(processor)
                 .addUrl(url)
                 .addPipeline(articleDbPipeline)
-                .thread(threadNum);
+                .thread(threadNum)
+                .setDownloader(httpClientDownloader);
         voteSpider.run();
     }
+
 }
--- big-ticket-web/src/main/webapp/index.jsp
+++ big-ticket-web/src/main/webapp/index.jsp
@@ -1,9 +1,21 @@
+<!DOCTYPE html>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Hello</title>
-</head>
-<body>
-hello
-</body>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="format-detection" content="email=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link href="img/easy.ico" rel="shortcut icon">
+    <link href="img/easy.ico" rel="apple-touch-icon">
+    <title>easy虎扑|更方便快捷的发倔热点</title>
+    <link href="dist/app.css" rel="stylesheet"></head>
+<body ontouchstart>
+<div id="container"></div>
+<script type="text/javascript" src="dist/vendor.js"></script>
+<script type="text/javascript" src="dist/app.js"></script></body>
 </html>
